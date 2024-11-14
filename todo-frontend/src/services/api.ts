fcb94d3e09d1396c1abbe5df35b573a88fffbd5f
Dev Gomes <quizueiramusj@gmail.com>
2024-11-14 14:31:57 -0300

    fix: api.ts

--- todo-frontend/src/services/api.ts
+++ todo-frontend/src/services/api.ts
@@ -1,11 +1,9 @@
-// src/services/api.ts
 import { Task } from '../types/Task';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://web-production-4c23.up.railway.app/api/tasks/",
+  baseURL: "https://web-production-4c23.up.railway.app/api",
 });
-
 
 export default api;
 
