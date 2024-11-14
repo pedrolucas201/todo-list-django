8ff3c40b8578c010ee34be26f63ef73b541d58fd
Dev Gomes <quizueiramusj@gmail.com>
2024-11-14 14:43:23 -0300

    fix: app.tsx

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -1,27 +1,37 @@
 // src/App.tsx
 import React, { useEffect, useState } from 'react';
-import TaskList from './components/TaskList';
-import TaskForm from './components/TaskForm';
 import { fetchTasks } from './services/api';
 import { Task } from './types/Task';
+import TaskList from './components/TaskList';
+import './global.css';
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
 
+  // Função para carregar as tarefas
   const loadTasks = async () => {
-    const response = await fetchTasks();
-    setTasks(response.data);
+    try {
+      const response = await fetchTasks();
+      setTasks(response.data); // Armazena as tarefas no estado
+    } catch (error) {
+      console.error("Erro ao buscar tarefas:", error);
+    }
   };
 
+  // Carrega as tarefas quando o componente monta
   useEffect(() => {
     loadTasks();
   }, []);
 
+  // Atualiza as tarefas após a exclusão
+  const handleTaskDeleted = () => {
+    loadTasks();
+  };
+
   return (
-    <div className="container">
-      <h1>Task Manager</h1>
-      <TaskForm onTaskAdded={loadTasks} />
-      <TaskList tasks={tasks} onTaskDeleted={loadTasks} />
+    <div className='container'>
+      <h1>Minha Lista de Tarefas</h1>
+      <TaskList tasks={tasks} onTaskDeleted={handleTaskDeleted} />
     </div>
   );
 };
